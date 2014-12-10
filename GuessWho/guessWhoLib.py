dc4ac661c68288a7c07a85829f2783d58634f62f
nthreadgold7489 <nthreadgold7489@sohamcollege.org.uk>
2014-12-10 09:44:57 +0000

    10.12.14

--- GuessWho/guessWhoLib.py
+++ GuessWho/guessWhoLib.py
@@ -1,31 +1,36 @@
 
 #Library for GuessWho by NT
 
+hairColors = ["brown","blonde","ginger","grey","white","red"] #List of possible answers when validating hair color answers
+eyeColors = ["grey","brown","green","blue"] #List of possible answers for eye colors
+genders = ["f","female","m","male"] #Options for gender validation
 
-hairColors = ["brown","blonde","ginger","grey","white","red"]
-eyeColors = ["grey","brown","green","blue"]
-genders = ["f","female","m","male"]
 
-def getPicture(name):
+#Create function to take picture and save it to a given name
+def getPicture(name): 
     try:
-        with picamera.PiCamera() as camera:
-            camera.start_preview()
-            time.sleep(2)
-            camera.capture("{0}.jpeg".format(name))
-            camera.stop_preview()
-    except picamera.exc.PicameraMMALError:
-        print('Could not detect camera')
+        with picamera.PiCamera() as camera: #Renames camera module for ease of use
+            camera.start_preview() #Begin photo preview to allow camera to adjust settings
+            time.sleep(2) #Give cameras time to adjust
+            camera.capture("{0}.jpeg".format(name)) #Take photo and save to the given filename
+            camera.stop_preview() #End process
+    except picamera.exc.PicameraMMALError: #Catch error, then output error message
+        print('Could not detect camera') 
 
 
-def validate(question,possAnswers):  #Defines function that check if input (inp) for the question (question) has been entered.
+
+#Defines function that check if input (inp) for the question (question) has been entered.
+def validate(question,possAnswers):  
     inp=""
-    while inp not in possAnswers:
+    while inp not in possAnswers: #Loop until input is valid
             inp=input(str(question))
             inp=inp.lower()
     
+#Defines function to get characteristics of user, asking them each question until a valid input is given.
+def getCharProfile():   .
 
-def getCharProfile():   #Defines function to get characteristics of user, asking them each question until a valid input is given.
-    name=input('What is your name?')
+    #Get user input for variety of features, doing basic validation for each feature
+    name=input('What is your name?') 
     hairColor=validate('What is your hair color?',hairColors)
     hat=validate('Do you have a hat?',['y','n','yes','no'])
     eyes=validate('What color are your eyes?',eyeColors)
@@ -33,6 +38,8 @@ def getCharProfile():   #Defines function to get characteristics of user, asking
     facialHair=validate('Do you have any facial hair?',['y','n','yes','no'])
     glasses=('Do you wear glasses?',['y','n','yes','no'])
 
+
+    #Change 'yes/no' answers to booleans for ease of use later on
     if hat == 'y' or hat=='yes':
         hat=True
     elif hat =='n' or hat == 'no':
@@ -47,6 +54,7 @@ def getCharProfile():   #Defines function to get characteristics of user, asking
         glasses =True
     elif glasses =='n' or glasses == 'no':
         glasses=False
-    
+
+    #Put all features into a list
     charProfile=(hairColor,hat,name,eyes,gender,facialHair,glasses)
-    return charProfile
+    return charProfile #Return list so it can be used by other functions
