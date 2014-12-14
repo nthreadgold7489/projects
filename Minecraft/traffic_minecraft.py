d6437a3aee51f541a54c05e69283f313601a9c21
nthreadgold7489 <nthreadgold7489@users.noreply.github.com>
2014-12-14 19:19:08 +0000

    Update traffic_minecraft.py

--- Minecraft/traffic_minecraft.py
+++ Minecraft/traffic_minecraft.py
@@ -6,23 +6,23 @@ time.sleep(3)
 
 black=7
 
-def red(x,y,z):
+def red(x,y,z): #Function to turn lights at certain coordinates (as defined by the parameters) to red
     mc.setBlock(x,float(y+5),z,35,14)
 
-def amber(x,y,z):
+def amber(x,y,z):#Function to turn lights at certain coordinates (as defined by the parameters) to amber
     mc.setBlock(x,float(y+4),z,35,1)
 
-def green(x,y,z):
+def green(x,y,z):#Function to turn lights at certain coordinates (as defined by the parameters) to green
     mc.setBlock(x,float(y+3),z,35,5)
     
-def reset(x,y,z):
+def reset(x,y,z):#Function to turn lights at certain coordinates (as defined by the parameters) back to ordinary towers of black blocks
     for i in range(0,6):
         mc.setBlock(x,y+i,z,35,black)
    
     
-
+#Create trafic lights and keep rotating through pattern of light changes automatically with specified waiting periods in between
 def traffic(x,y,z):
-    while True:
+    while True: #Constant looping 
         reset(x,y,z)
         red(x,y,z)
         time.sleep(5)
@@ -38,20 +38,13 @@ def traffic(x,y,z):
         time.sleep(3.5)
         reset(x,y,z)
     else:
-        mc.postToChat('Issue with traffic lights.')
+        mc.postToChat('Issue with traffic lights.') #Alert user is there is a problem
 
 
 
 
-x,y,z=mc.player.getPos()
-traffic(x,y,z)
-time.sleep(2)
-x1,y1,z1=mc.player.getPos()
-traffic(x1,y1,z1)
-time.sleep(2)
-x2,y2,z2=mc.player.getPos()
-traffic(x2,y2,z2)
-time.sleep(2)
+x,y,z=mc.player.getPos() 
+traffic(x,y,z) #Calls traffic function on players coordinates.
 
     
     
