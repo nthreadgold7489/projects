9177c37efcda54c830e6914e2f26fa2ea897e0d4
nthreadgold7489 <nthreadgold7489@users.noreply.github.com>
2014-12-14 16:08:13 +0000

    Update capture_validate.py

--- GuessWho/capture_validate.py
+++ GuessWho/capture_validate.py
@@ -3,27 +3,27 @@
 import time,picamera #imports necessary modules
 
 name=""
-while name == "":
-    name=input('Photo name:') #Gets file name and makes sure it is longer than one character
+while name == "": #Starts potentially infinite loop
+    name=input('Photo name:') #Make sure file name is longer than one character (loops until it is)
 
 def capture(name): #Defines function for capturing image
-    with picamera.PiCamera() as camera:
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture(str(name))
-        camera.stop_preview()
+    with picamera.PiCamera() as camera: #Change name of camera module and use with keyword for safe shutdown after camera module has been used
+        camera.start_preview() #Begin preview to allow camera time to adjust to surroundings
+        time.sleep(2) #Allow camera time to adjust to surroundings
+        camera.capture('{0}.jpg'.format(name))  #Capture photo and save it as given name
+        camera.stop_preview() #End capturing process
 
-check=""
+
 
 def validate_capture(x):  #Defines function for capturing image, validating if it is okay input and retaking photo if the user dislikes it.
     capture(x)
-    while check != 'yes' or check!='no':
-        check=input('Is it okay?')
-        check=check.lower()
-    if check== 'yes':
-        print('Saved')
+    while check != 'yes' or check!='no': #Loop until user says the photo is either okay or not.
+        check=input('Is it okay?') #Get user input
+        check=check.lower() #make it lower case so only have to compare it to 'yes' rather than 'yes' and 'YES'.
+    if check== 'yes': # Compare input to yes
+        print('Saved') # If the user is happy, don't overwrite photo
     elif check == 'no':
-        capture(str(name))
+        capture(str(name)) # Take another photo and overwrite previous one if user doesn't like it
     
         
-validate_capture(name)
+validate_capture(name) #Test photo and validation check
